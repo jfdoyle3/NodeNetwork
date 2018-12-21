826bd610bb2ed0c09a997f5aa474ba9640c33e57
Jim Doyle <indellibleink@gmail.com>
2018-12-21 08:17:29 -0500

    User can Register using form.

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,6 +1,23 @@
 import React, { Component } from 'react'
 
 class Login extends Component {
+  constructor(){
+    super();
+    this.state={
+        email: '',
+        password: '',
+        errors:{}
+    }
+    this.onChange=this.onChange.bind(this)
+    this.onSubmit=this.onSubmit.bind(this)
+}
+onChange(e) {
+    this.setState({[e.target.name]: e.target.value})
+}
+onSubmit(e) {
+    e.preventDefault();
+
+}
   render() {
     return (
         <div className="login">
@@ -9,12 +26,12 @@ class Login extends Component {
             <div className="col-md-8 m-auto">
               <h1 className="display-4 text-center">Log In</h1>
               <p className="lead text-center">Sign in to your Node Network account</p>
-              <form action="dashboard.html">
+              <form onSubmit={this.onSubmit}>
                 <div className="form-group">
-                  <input type="email" className="form-control form-control-lg" placeholder="Email Address" name="email" />
+                  <input type="email" className="form-control form-control-lg" placeholder="Email Address" value={this.state.email} name="email" />
                 </div>
                 <div className="form-group">
-                  <input type="password" className="form-control form-control-lg" placeholder="Password" name="password" />
+                  <input type="password" className="form-control form-control-lg" placeholder="Password" value={this.state.pasword} name="password" />
                 </div>
                 <input type="submit" className="btn btn-info btn-block mt-4" />
               </form>
