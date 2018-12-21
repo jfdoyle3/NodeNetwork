acbc83a9897c33dd227d702976e806e00b8fb5a7
Jim Doyle <indellibleink@gmail.com>
2018-12-20 21:09:15 -0500

    Added BrowserRouter and  Link to connect routes and components.

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,31 @@
+//Add ins
 import React, { Component } from "react";
-import "./App.css";
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+
+//Components
 import Navbar from "./components/layout/Navbar"
 import Landing from "./components/layout/Landing"
 import Footer from "./components/layout/Footer"
+import Login from "./components/auth/Login"
+import Register from "./components/auth/Register"
 
-
+// Style Sheet
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className="App">
         <Navbar />
-        <Landing />
+        <Route exact path="/" component={Landing} />
+        <div className="container">
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/loginro" component={Login} />
+        </div>
         <Footer />
       </div>
+      </Router>
     );
   }
 }
