af104c96657ef76dab5535fa142b547d6f41a893
Jim Doyle <indellibleink@gmail.com>
2018-12-22 07:50:36 -0500

    added redux and ran npm update to update packages.

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 //Add ins
 import React, { Component } from "react";
 import {BrowserRouter as Router, Route} from 'react-router-dom'
+import {Provider} from 'react-redux'
 
 //Components
 import Navbar from "./components/layout/Navbar"
