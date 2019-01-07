2e74b3e4c3a441e1651e289183f93f40f80665f1
Jim Doyle <indellibleink@gmail.com>
2019-01-07 06:11:12 -0500

    finished with Profile page: User:s can add, edit, delete

--- client/src/App.js
+++ client/src/App.js
@@ -25,6 +25,7 @@ import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import Dashboard from "./components/dashboard/Dashboard";
 import CreateProfile from "./components/create-profile/CreateProfile";
+import EditProfile from "./components/edit-profile/EditProfile";
 
 // Style Sheet
 import "./App.css";
@@ -62,6 +63,13 @@ class App extends Component {
                   exact
                   path="/create-profile"
                   component={CreateProfile}
+                />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/edit-profile"
+                  component={EditProfile}
                 />
               </Switch>
             </div>
