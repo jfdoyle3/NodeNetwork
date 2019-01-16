67322cc2bdd5638878213318acac3f655e7d6a31
Jim Doyle <indellibleink@gmail.com>
2019-01-16 11:13:39 -0500

    Created Add Experience form

--- client/src/App.js
+++ client/src/App.js
@@ -26,6 +26,7 @@ import Register from "./components/auth/Register";
 import Dashboard from "./components/dashboard/Dashboard";
 import CreateProfile from "./components/create-profile/CreateProfile";
 import EditProfile from "./components/edit-profile/EditProfile";
+import AddExperience from "./components/add-credentials/AddExperience";
 
 // Style Sheet
 import "./App.css";
@@ -70,6 +71,13 @@ class App extends Component {
                   exact
                   path="/edit-profile"
                   component={EditProfile}
+                />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/add-experience"
+                  component={AddExperience}
                 />
               </Switch>
             </div>
--- client/src/components/add-credentials/AddExperience.js
+++ client/src/components/add-credentials/AddExperience.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link, withRouter} from 'react-router-dom';
-import {TextFieldGroup} from '../common/TextFieldGroup';
-import {TextAreaGroup} from '../common/TextAreaGroup';
+import TextFieldGroup from '../common/TextFieldGroup';
+import TextAreaGroup from '../common/TextAreaGroup';
 import {connect} from 'react-redux';
 import Prop-types from 'prop-types';
 
@@ -20,12 +20,31 @@ import Prop-types from 'prop-types';
       error: {},
       disable: false
       };
+      this.onChange=this.onChange.bind(this);
+      this.onSubmit=this.onSubmit.bind(this);
+      this.onCheck=this.onCheck.bind(this);
+   }
+
+   onSubmit(e){
+     e.preventDefault();
+     
+   }
+
+   onChange(e){
+     this.setState({[e.target.name]:e.target.value});
+   }
+
+   oncheck(e){
+     this.setState({
+       disable: !this.state.disabled,
+       current: !this.state.current
+     });
    }
   render(){
     const {errors}=this.state;
   return (
   <div>
-   <div className=" add-experience">
+   <div className="add-experience">
     <div className="container">
      <div className="row">
            <div className="col-md-8 m-auto">
@@ -33,45 +52,72 @@ import Prop-types from 'prop-types';
           <h1 className="display-4 text-center">Add Your Experience</h1>
           <p className="lead text-center">Add any developer/programming positions that you have had in the past</p>
           <small className="d-block pb-3">* = required field</small>
-          <form action="add-education.html">
-            <div className="form-group">
-              <input type="text" className="form-control form-control-lg" placeholder="* Job Title" name="title" required />
-            </div>
-            <div className="form-group">
-              <input type="text" className="form-control form-control-lg" placeholder="* Company" name="company" required />
-            </div>
-            <div className="form-group">
-              <input type="text" className="form-control form-control-lg" placeholder="Location" name="location" />
-            </div>
+          <form onSubmit={this.onSubmit}>
+            <TextFieldGroup
+              placeholder="* Job Title"
+              name="title"
+              value={this.state.title}
+              onChange={this.onChange}
+              error={errors.title}
+            />
+            <TextFieldGroup
+              placeholder="* Company"
+              name="company"
+              value={this.state.company}
+              onChange={this.onChange}
+              error={errors.company}
+            />
+            <TextFieldGroup
+              placeholder="Location"
+              name="location"
+              value={this.state.location}
+              onChange={this.onChange}
+              error={errors.location}
+            />
             <h6>From Date</h6>
-            <div className="form-group">
-              <input type="date" className="form-control form-control-lg" name="from" />
-            </div>
+            <TextFieldGroup
+              name="from"
+              value={this.state.from}
+              onChange={this.onChange}
+              error={errors.from}
+            />            
             <h6>To Date</h6>
-            <div className="form-group">
-              <input type="date" className="form-control form-control-lg" name="to" />
-            </div>
+            <TextFieldGroup
+              name="to"
+              value={this.state.to}
+              onChange={this.onChange}
+              error={errors.to}
+              disabled={this.state.disabled ? 'disable': ""}
+            />
             <div className="form-check mb-4">
-              <input className="form-check-input" type="checkbox" name="current" value="" id="current" />
-              <label className="form-check-label" for="current">
+              <input 
+                className="form-check-input"
+                type="checkbox"
+                name="current"
+                value={this.state.current} checked={this.state.current}
+                onChange={this.onCheck}
+                id="current"
+              />
+              <label className="form-check-label" htmlFor="current">
                 Current Job
               </label>
             </div>
-            <div className="form-group">
-              <textarea className="form-control form-control-lg" placeholder="Job Description" name="description"></textarea>
-              <small className="form-text text-muted">Some of your responsabilities, etc</small>
-            </div>
-            <input type="submit" className="btn btn-info btn-block mt-4" />
+            <TextFieldGroup
+              placeholder="Job Descriptio"
+              name="description"
+              value={this.state.description}
+              onChange={this.onChange}
+              error={errors.description}
+              info="Some of your responsabilities, etc"
+            />
+            <input type="submit" value="Submit" className="btn btn-info btn-block mt-4" />
           </form>
         </div>
       </div>
     </div>
   </div>
          </div>
-         </div>
-         </div>
-         </div>
-         </div>
+
           
      )
    }
