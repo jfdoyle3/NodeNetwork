eafd952b0a26f32e48ef3888253c262cd774d2af
Jim Doyle <indellibleink@gmail.com>
2019-02-06 07:44:59 -0500

    Working on ProfileItems

--- client/src/components/profiles/ProfileItem.js
+++ client/src/components/profiles/ProfileItem.js
@@ -14,6 +14,21 @@ class ProfileItem extends Component {
           </div>
           <div className="col-lg-6 col-md-4 col-8">
             <h3>{profile.user.name}</h3>
+            <p>
+              {profile.status}
+              {isEmpty(profile.company) ? null : (
+                <span> at {profile.company}</span>
+              )}
+            </p>
+            <p>
+              {isEmpty(profile.location) ? null : (
+                <span>{profile.location}</span>
+              )}
+            </p>
+            <Link to={`/profile/${profile.handle}`} className="btn btn-info">
+              View Profile
+            </Link>
+            <div className="col-md-4"
           </div>
         </div>
       </div>
