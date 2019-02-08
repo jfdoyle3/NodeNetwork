ebb380c6e295ffc9713f30f3c27c6d81b2e15329
Jim Doyle <indellibleink@gmail.com>
2019-02-07 21:12:35 -0500

    added education and experience to profile page

--- client/src/components/profile/ProfileCreds.js
+++ client/src/components/profile/ProfileCreds.js
@@ -4,16 +4,15 @@ import Moment from "react-moment";
 class ProfileCreds extends Component {
   render() {
     const { experience, education } = this.props;
-
     const expItems = experience.map(exp => (
       <li key={exp._id} className="list-group-item">
         <h4>{exp.company}</h4>
         <p>
-          <Moment format="YYYY/MM/DD">{exp.from}</Moment> -
+          <Moment format="YYYY/MM/DD">{exp.from} </Moment> -
           {exp.to === null ? (
             "Now"
           ) : (
-            <Moment format="YYYY/MM/DD">exp.to}</Moment>
+            <Moment format="YYYY/MM/DD">{exp.to}</Moment>
           )}
         </p>
         <p>
@@ -46,7 +45,7 @@ class ProfileCreds extends Component {
           {edu.to === null ? (
             "Now"
           ) : (
-            <Moment format="YYYY/MM/DD">edu.to}</Moment>
+            <Moment format="YYYY/MM/DD">{edu.to}</Moment>
           )}
         </p>
         <p>
@@ -54,7 +53,7 @@ class ProfileCreds extends Component {
           {edu.degree}
         </p>
         <p>
-          ><strong>Field Of Study: </strong>
+          <strong>Field Of Study: </strong>
           {edu.fieldofstudy}
         </p>
         <p>
@@ -89,5 +88,4 @@ class ProfileCreds extends Component {
     );
   }
 }
-
 export default ProfileCreds;
