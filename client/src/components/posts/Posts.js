4a4450ba342f47c53c2d5314ed5af561300642aa
Jim Doyle <indellibleink@gmail.com>
2019-02-09 22:52:46 -0500

    updated posts

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -1,9 +1,19 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import PostForm from "./PostForm";
+import Spinner from "../common/Spinner";
 
 class Posts extends Component {
   render() {
     return (
-      <div>
+      <div className="feed">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12" />
+            <PostForm />
+          </div>
+        </div>
         <h2>boo</h2>
       </div>
     );
