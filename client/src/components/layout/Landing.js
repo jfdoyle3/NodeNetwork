1bf57322c5e8e0403adfb645c48f78a60f1b28f6
Jim Doyle <indellibleink@gmail.com>
2019-02-13 21:24:03 -0500

    changed text

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -17,7 +17,7 @@ class Landing extends Component {
             <div className="row">
               <div className="col-md-12 text-center">
                 <h1 className="display-3 mb-4">Node Network</h1>
-                <p className="lead">Connecting devs one node at a time</p>
+                <p className="lead">Connecting coders one node at a time</p>
                 <hr />
                 <Link to="/register" className="btn btn-lg btn-info mr-2">
                   Sign Up
